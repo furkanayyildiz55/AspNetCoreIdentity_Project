107bb5396abfcb9f35264f6d109490e3a38d6cec
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-05-03 16:15:45 +0300

    Admin areası oluşturuldu ve yeni areada kullanıcılar listelendi

---
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCoreIdentity.Web.Areas.Admin.Models;
+using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public partial class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> _UserManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _UserManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _UserManager.Users.ToListAsync();
+
+            var userViewModelList = userList.Select(x => new UserViewModel() {
+                Email = x.Email,
+                Name = x.UserName,
+                Id = x.Id,
+            }).ToList();
+
+            return View(userViewModelList);
+        }
+
+    }
+}
