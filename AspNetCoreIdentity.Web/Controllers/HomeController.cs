e64a62bd881eaf28717517d8aeffeb3129c04ae0
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-05-03 15:44:05 +0300

    SignUp işlemlerinin yapılması

--- AspNetCoreIdentity.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentity.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using AspNetCoreIdentity.Web.Models;
+using AspNetCoreIdentity.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Diagnostics;
 
 namespace AspNetCoreIdentity.Web.Controllers
@@ -8,10 +11,54 @@ namespace AspNetCoreIdentity.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //Readonly ile sadece oluþturulduðu anda veya constructor tarafýnda deðer atamasý yapýlabilir.
+        //UserManager bizim için kullanýcý ile ilgili tüm iþlemleri yapar 
+        private readonly UserManager<AppUser> _UserManager;
+
+        public HomeController(ILogger<HomeController> logger , UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _UserManager = userManager;
         }
+
+        #region SignUp
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+
+            if (!ModelState.IsValid)
+                return View();
+
+            AppUser appUser = new AppUser()
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.Phone,
+            };
+
+            var identityResult = await _UserManager.CreateAsync(appUser , request.Password);
+            if (identityResult.Succeeded)
+            {
+                //Kullanýcý kaydý baþarýlý olmasý durumunda SuccessMessage kaybetmek istemiyoruz bu nedenle TempData içerisinde tutuyoruz
+                TempData["SuccessMessage"] = "Kullanýcý baþarýyla oluþturuldu."; 
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+
+            //hata durumunda
+            foreach (var error in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View();
+        }
+        #endregion
+
+        #region Idnex Privacy Error
 
         public IActionResult Index()
         {
@@ -28,5 +75,7 @@ namespace AspNetCoreIdentity.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #endregion
     }
 }
