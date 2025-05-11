13a1e646f23f3ef32eb95e58c842fe3dbb90fecf
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-05-11 20:34:53 +0300

    Role güncelleme sayfası eklendi

--- AspNetCoreIdentity.Web/Areas/Admin/Controllers/RolesController.cs
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/RolesController.cs
@@ -57,5 +57,37 @@ namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
         }
 
         #endregion
+
+        #region RoleUpdate
+
+        public async Task<IActionResult> RoleUpdate(string id)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(id);
+
+            if(roleToUpdate == null)
+                throw new Exception("Güncellenecek rol bulunamamıştır.");
+
+
+            return View(new RoleUpdateViewModel() { Id = roleToUpdate.Id, Name=roleToUpdate.Name} );
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleUpdate(RoleUpdateViewModel request)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(request.Id);
+            if (roleToUpdate == null)
+            {
+                throw new Exception("Güncellenecek rol bulunamamıştır.");
+            }
+
+            roleToUpdate.Name = request.Name;
+            await _roleManager.UpdateAsync(roleToUpdate);
+
+            ViewData["SuccessMessage"] = "Rol bilgisi güncellenmiştir";
+
+            return View();
+        }
+
+        #endregion
     }
 }
