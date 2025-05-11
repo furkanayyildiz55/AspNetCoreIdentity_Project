11b9e688d47d149fa6a55d9aecc6eec41a1091b8
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-05-11 20:07:53 +0300

    Role Ekleme sayfası oluşturuldu

---
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,48 @@
+﻿using AspNetCoreIdentity.Web.Areas.Admin.Models;
+using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentity.Web.Extensions;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RolesController(RoleManager<AppRole> roleManager , UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoleCreate ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleCreate(RoleCreateViewModel request)
+        {
+            var result = await _roleManager.CreateAsync(new AppRole() 
+            {
+                Name = request.Name,
+            });
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelErrorList(result.Errors);
+                return View();
+            }
+
+            return RedirectToAction(nameof(RolesController.Index));
+        }
+    }
+}
