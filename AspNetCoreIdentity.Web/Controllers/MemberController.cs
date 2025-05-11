3cefdbd5b6a2e8a3e51bb6b5da2acca119e529b0
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-05-11 11:50:53 +0300

    Kullanıcı şifre değiştirme sayfası yapıldı

--- AspNetCoreIdentity.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentity.Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using AspNetCoreIdentity.Web.Models;
+﻿using AspNetCoreIdentity.Web.Extensions;
+using AspNetCoreIdentity.Web.Models;
 using AspNetCoreIdentity.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -13,7 +14,7 @@ namespace AspNetCoreIdentity.Web.Controllers
         private readonly SignInManager<AppUser> _SignInManager;
         private readonly UserManager<AppUser> _UserManager;
 
-        public MemberController(SignInManager<AppUser> signInManager , UserManager<AppUser> userManager)
+        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
             _SignInManager = signInManager;
             _UserManager = userManager;
@@ -39,6 +40,45 @@ namespace AspNetCoreIdentity.Web.Controllers
             await _SignInManager.SignOutAsync();
         }
 
+        public async Task<IActionResult> PasswordChange()
+        {
+            return View();
+        }
 
-    }
+        [HttpPost]
+		public async Task<IActionResult> PasswordChange(PasswordChangeViewModel request)
+		{
+            if (!ModelState.IsValid)
+                return View();
+
+            var currentUser = await _UserManager.FindByNameAsync(User.Identity!.Name);
+			var checkOldPassword = await _UserManager.CheckPasswordAsync(currentUser, request.PasswordOld);
+
+			if (!checkOldPassword)
+			{
+				ModelState.AddModelError(string.Empty, "Eski şifreniz yanlış");
+				return View();
+			}
+
+			var resultChangePassword = await _UserManager.ChangePasswordAsync(currentUser, request.PasswordOld, request.PasswordNew);
+
+            if (!resultChangePassword.Succeeded)
+            {
+				ModelState.AddModelErrorList(resultChangePassword.Errors.Select(x => x.Description).ToList());
+				return View();
+			}
+
+			//Security stamp güncellenerek diğer cihazlardan çıkış yapılır
+			await _UserManager.UpdateSecurityStampAsync(currentUser);
+
+			//Çıkış ve ardından tekrar giriş yapma işlemi ile cookie güncellenir
+			await _SignInManager.SignOutAsync();
+            await _SignInManager.PasswordSignInAsync(currentUser, request.PasswordNew, true, false);
+
+			TempData["SuccessMessage"] = "Şifreniz başarıyla değiştirilmiştir";
+			return View();
+		}
+
+
+	}
 }
